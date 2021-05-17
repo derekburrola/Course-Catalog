7d913ca5a45a1f8fe265c8f6aab70523c1bf9c68
Der3k Burrola <der3kburrola@gmail.com>
2021-05-17 16:15:00 -0600

    Initial Commit

---
+++ Course CatalogTests/Course_CatalogTests.swift
@@ -0,0 +1,33 @@
+//
+//  Course_CatalogTests.swift
+//  Course CatalogTests
+//
+//  Created by Der3k Burrola on 5/17/21.
+//
+
+import XCTest
+@testable import Course_Catalog
+
+class Course_CatalogTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
